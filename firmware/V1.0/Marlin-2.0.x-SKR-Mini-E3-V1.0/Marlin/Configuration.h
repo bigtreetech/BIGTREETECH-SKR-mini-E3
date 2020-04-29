7e23ae8952d154a1cbef071887e48cff5702325a
chzj333 <872543189@qq.com>
2020-04-29 15:28:39 +0800

    Compile with STM32F103RC_btt_512K

--- firmware/V1.0/Marlin-2.0.x-SKR-Mini-E3-V1.0/Marlin/Configuration.h
+++ firmware/V1.0/Marlin-2.0.x-SKR-Mini-E3-V1.0/Marlin/Configuration.h
@@ -988,7 +988,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points`
+#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
