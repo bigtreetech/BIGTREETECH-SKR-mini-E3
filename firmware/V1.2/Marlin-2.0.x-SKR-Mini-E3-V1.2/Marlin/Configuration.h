aaa74128fd6316f8833678ec43695ec123806a66
manufactory-dave <69147972+manufactory-dave@users.noreply.github.com>
2020-08-03 13:43:12 +0100

    Update Configuration.h

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -18,6 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * Manufactory Dave's clone for SKR Mini E3 on an Xvico X3 Pro
  */
 #pragma once
 
