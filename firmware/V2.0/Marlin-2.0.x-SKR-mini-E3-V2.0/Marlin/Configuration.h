205d6ec5275d0b8a90f0ce38918026f8abff10a4
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-10-07 19:46:58 -0700

    Increase EXTRUDE_MAXLENGTH to 600

--- firmware/V2.0/Marlin-2.0.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -572,7 +572,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 600
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
